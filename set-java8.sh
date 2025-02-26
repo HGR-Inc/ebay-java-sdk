ef5a83b63983d765fbb90a2290f29eb18be616a7
Nacho V. <96270672+gabrielvcap@users.noreply.github.com>
2025-02-26 17:57:45 -0500

    old sdk

--- set-java8.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export JAVA_HOME=/Users/nachocodev/Library/Java/JavaVirtualMachines/corretto-1.8.0_422/Contents/Home
-export PATH=$JAVA_HOME/bin:$PATH
-echo "Java 8 environment set up successfully!"
-java -version
